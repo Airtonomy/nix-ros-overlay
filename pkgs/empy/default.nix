ac1f64b733f00b59592c803749f231fb4b638669
Alex Elliott <alex.elliott@airtonomy.ai>
2022-10-12 13:57:39 -0500

    use fetchPyPi

--- pkgs/empy/default.nix
+++ pkgs/empy/default.nix
@@ -1,14 +1,16 @@
-{ lib, fetchurl, buildPythonPackage }:
+{ lib
+, buildPythonPackage
+, fetchPyPi }:
 
 buildPythonPackage rec {
   pname = "empy";
   version = "3.3.4";
 
-  src = fetchurl {
-    url = "http://www.alcyone.com/software/empy/empy-${version}.tar.gz";
-    sha256 = "1cgikljjcqxgz168prpvb0bnirbdxf9wmgj0vlzzhzzwf4a229li";
+  src = fetchPyPi {
+    inherit pname version;
+    sha256 = "73ac49785b601479df4ea18a7c79bc1304a8a7c34c02b9472cf1206ae88f01b3";
   };
-
+ 
   meta = {
     description = "A powerful and robust templating system for Python";
     homepage = http://www.alcyone.com/software/empy/;
