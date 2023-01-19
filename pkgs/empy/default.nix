f220dc70cb575becc90b4dd2cee6e4a73d1536a0
Ahmed Elsaharti <ahmed.elsaharti@und.edu>
2023-01-19 06:37:51 -0800

    fix empy

--- pkgs/empy/default.nix
+++ pkgs/empy/default.nix
@@ -1,16 +1,17 @@
-{ lib, fetchurl, buildPythonPackage }:
+{ lib, fetchPypi, buildPythonPackage }:
 
 buildPythonPackage rec {
   pname = "empy";
   version = "3.3.4";
-
-  src = fetchurl {
-    url = "http://www.alcyone.com/software/empy/empy-${version}.tar.gz";
-    sha256 = "1cgikljjcqxgz168prpvb0bnirbdxf9wmgj0vlzzhzzwf4a229li";
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "c6xJeFtgFHnfTqGKfHm8EwSop8NMArlHLPEgauiPAbM=";
   };
-
-  meta = {
-    description = "A powerful and robust templating system for Python";
-    homepage = http://www.alcyone.com/software/empy/;
+  pythonImportsCheck = [ "em" ];
+  meta = with lib; {
+    homepage = "http://www.alcyone.com/software/empy/";
+    description = "A templating system for Python.";
+    maintainers = with maintainers; [ nkalupahana ];
+    license = licenses.lgpl21Only;
   };
 }
